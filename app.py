3d6ff2596f513dc7fd043e00a7fbf3439201698e
Danov K <boss_danov_1337@google.ru>
2023-08-07 19:07:00 +0300

    Добавил класс дополнительного виджета для создания сотрудника

--- app.py
+++ app.py
@@ -2,16 +2,44 @@
 import sys
 from random import randint
 
-from PySide6.QtWidgets import QMainWindow, QApplication, QHeaderView
+from PySide6.QtWidgets import QMainWindow, QApplication, QHeaderView, QWidget
 
+#макеты окон
 from first_maket_ui import Ui_Form
+import addEmployee_maket_ui as addEmplWindow
+
+#интерфейсы для вьюшек и сами вьюшки
 from employeModels import EmployeeInterface, EmployeeView
 from reportModels import ReportInterface, ReportView
+
+#основные классы сотрудников и отчетов
+from logicModels import Employee, Specialist, Report
+
+
+class AddEmployeeWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.mainWidget = parent
+
+        self.ui = addEmplWindow.Ui_Form()
+        self.ui.setupUi(self)
+
+        self.ui.employeeTypeBox.setId(self.ui.isStandart, 1)
+        self.ui.employeeTypeBox.setId(self.ui.isStudent, 2)
+        self.ui.employeeTypeBox.setId(self.ui.isNurse, 3)
+        self.ui.employeeTypeBox.setId(self.ui.isDoctor, 4)
+
+    def createEmployeeTouched(self):
+        if self.mainWidget is not None:
+            print(self.ui.employeeTypeBox.checkedId())
+        
 
 
 class ProgramWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.addEmployeeWindow = None
 
         self.ui = Ui_Form()
         self.ui.setupUi(self)
@@ -35,8 +63,14 @@ class ProgramWindow(QMainWindow):
 
 
     def addEmployeePressed(self):
-        self.employes.append(EmployeeView(room=randint(0,1000), percent=randint(0,100)))
-        self.ui.employesTable.setModel(EmployeeInterface(self.employes))
+        if self.addEmployeeWindow is None:
+            self.addEmployeeWindow = AddEmployeeWidget(parent=self)
+            self.addEmployeeWindow.show()
+        else:
+            self.addEmployeeWindow.close()
+            self.addEmployeeWindow = None
+
+        #self.ui.employesTable.setModel(EmployeeInterface(self.employes))
 
 
     def addReportPressed(self):
